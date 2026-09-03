521fa1ee68d4f176e2405819545322d530b55d3a
lahrech381 <lahrech381@gmail.com>
2026-09-03 20:22:08 +0200

    fix: add basePath for GitHub Pages CSS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/midad-alt",
+  assetPrefix: "/midad-alt",
   images: {
     unoptimized: true,
   },
